ec83c2342006f6fdabe718ea0ab728a60bc39113
eparker12 <edward.parker@lshtm.ac.uk>
2022-04-29 12:36:16 +0100

    Updated Table 1 VE

--- analysis/VE/table_1_VE.R
+++ analysis/VE/table_1_VE.R
@@ -51,56 +51,75 @@ data_cohort <- read_rds(here::here("output", "data", input_name))
 
 ## Format data
 data_cohort <- data_cohort %>%
-  mutate(N=1,
-         time_between_vaccinations1_2 = as.character(cut(tbv1_2,
-                                                         breaks = c(0, 42, 70, 98, Inf),
-                                                         labels = c("6 weeks or less", "6-10 weeks", "10-14 weeks", "14 weeks or more"),
-                                                         right = FALSE)),
-         smoking_status = ifelse(is.na(smoking_status), "N&M", smoking_status),
-         bpcat = ifelse(bpcat=="High" | bpcat=="Elevated", 1, 0)
+  mutate(
+    N = 1,
+    #allpop = "All",
+    # Calculate time between vaccinations 1-2
+    time_between_vaccinations1_2 = as.character(cut(tbv1_2,
+                                                    breaks = c(0, 42, 56, 70, 84, 98, Inf),
+                                                    labels = c("6 weeks or less", "6-8 weeks", "8-10 weeks", "10-12 weeks", "12-14 weeks", "14+ weeks"),
+                                                    right = FALSE)),
+    # Calculate time between vaccinations 2-3
+    time_between_vaccinations2_3 = as.character(cut(tbv2_3,
+                                                    breaks = c(0, 84, 168, 252, Inf),
+                                                    labels = c("12 weeks or less", "12-24 weeks", "24-36 week", "36+ weeks"),
+                                                    right = FALSE)),
+    smoking_status = ifelse(is.na(smoking_status), "N&M", smoking_status),
+    bpcat = ifelse(bpcat=="High" | bpcat=="Elevated", 1, 0)
   ) %>%
-  mutate(smoking_status = ifelse(smoking_status=="S&E", 1, 0),
-         time_between_vaccinations1_2 = ifelse(is.na(vax2_date), "Not applicable (no dose given)", time_between_vaccinations1_2)
+  mutate(
+    smoking_status = ifelse(smoking_status=="S&E", 1, 0),
+    time_between_vaccinations1_2 = ifelse(is.na(vax2_date), "Not applicable (no dose given)", time_between_vaccinations1_2),
+    time_between_vaccinations2_3 = ifelse(is.na(vax3_date), "Not applicable (no dose given)", time_between_vaccinations2_3)
   )
 
 ## baseline variables
 counts0 <- data_cohort %>%
   select(vax12_type,
          N,
-         ageband2, 
+         #allpop,
+         ## Factors associated with attitudes/confidence
+         ageband2,
+         care_home,
+         hscworker,
+         housebound,
+         endoflife,
+         rural_urban_group,
+         
+         ## Factors associated with attitudes/confidence
          sex,
          ethnicity,
          imd,
+         prior_covid_cat,
+         
+         ## Clinical risk group (CKD-related)
+         ckd_5cat,
+         ckd_7cat,
+         #removed: dialysis, kidney_transplant
+         
+         ## Clinical risk group (non-CKD-related)
+         immunosuppression, 
+         mod_sev_obesity,
+         diabetes,
+         any_resp_dis,
+         chd, 
+         cld,
+         asplenia,
+         cancer,
+         haem_cancer,
+         non_kidney_transplant,
+         chronic_neuro_dis_inc_sig_learn_dis,
+         sev_mental_ill,
+         cev_other,
+         #removed: smoking_status, asthma, bpcat
+         
+         ## Other descriptors of interest
          region,
          jcvi_group,
-         rural_urban_group,
-         chronic_kidney_disease_diagnostic,
-         dialysis, 
-         kidney_transplant, 
          chronic_kidney_disease_stages_3_5,
-         cev,
-         #care_home,
-         #hscworker,
-         #endoflife,
-         #housebound,
-         smoking_status,
-         asthma,
-         bpcat,
-         immunosuppression, 
-         chronic_resp_dis, 
-         diabetes, 
-         cld, 
-         chd, 
-         asplenia, 
-         cancer, 
-         haem_cancer,
-         obesity, 
-         chronic_neuro_dis_inc_sig_learn_dis, 
-         sev_mental_ill, 
-         non_kidney_transplant,
-         multimorb,
-         prior_covid_cat,
-         time_between_vaccinations1_2 
+         time_between_vaccinations1_2,
+         time_between_vaccinations2_3
+         #removed: any_ckd_flag
          ) %>%
   tbl_summary(by = vax12_type) 
 counts0$inputs$data <- NULL
@@ -122,31 +141,27 @@ table1$percent2 = round(table1$count2/sum(data_cohort$vax12_type=="pfizer-pfizer
 colnames(table1) = c("Group", "Variable", "Count_az", "Count_pfizer", "Percent_az", "Percent_pfizer")
 
 ## Relabel variables for plotting
-table1$Variable[table1$Variable=="cev"] = "Clinically extremely vulnerable"
+# Relabel variables for plotting
 table1$Variable[table1$Variable=="care_home"] = "Care home resident"
-table1$Variable[table1$Variable=="hscworker"] = "Health and social care worker"
-table1$Variable[table1$Variable=="endoflife"] = "End of life care"
+table1$Variable[table1$Variable=="hscworker"] = "Health/social care worker"
+table1$Variable[table1$Variable=="cev"] = "Clinically extremely vulnerable"
 table1$Variable[table1$Variable=="housebound"] = "Housebound"
-table1$Variable[table1$Variable=="chronic_kidney_disease_diagnostic"] = "CKD diagnostic code"
-table1$Variable[table1$Variable=="dialysis"] = "Dialysis"
-table1$Variable[table1$Variable=="kidney_transplant"] = "Kidney transplant"
-table1$Variable[table1$Variable=="chronic_kidney_disease_stages_3_5"] = "CKD stage 3-5 code"
-table1$Variable[table1$Variable=="smoking_status"] = "Current or former smoker"
-table1$Variable[table1$Variable=="asthma"] = "Asthma"
-table1$Variable[table1$Variable=="bpcat"] = "High or elevated blood pressure"
-table1$Variable[table1$Variable=="immunosuppression"] = "Immunosuppression"
-table1$Variable[table1$Variable=="chronic_resp_dis"] = "Chronic respiratory disease"
-table1$Variable[table1$Variable=="diabetes"] = "Diabetes"
-table1$Variable[table1$Variable=="cld"] = "Chronic liver disease"
-table1$Variable[table1$Variable=="chd"] = "Chronic heart disease"
-table1$Variable[table1$Variable=="asplenia"] = "Asplenia"
-table1$Variable[table1$Variable=="cancer"] = "Cancer"
-table1$Variable[table1$Variable=="haem_cancer"] = "Haematologic cancer"
-table1$Variable[table1$Variable=="obesity"] = "Obesity"
-table1$Variable[table1$Variable=="chronic_neuro_dis_inc_sig_learn_dis"] = "Chronic neurological disease (including learning disability)"
-table1$Variable[table1$Variable=="sev_mental_ill"] = "Severe mental illness"
-table1$Variable[table1$Variable=="non_kidney_transplant"] = "Organ transplant (non-kidney)"
+table1$Variable[table1$Variable=="endoflife"] = "End of life care"
 table1$Variable[table1$Variable=="prior_covid_cat"] = "Prior COVID"
+table1$Variable[table1$Variable=="immunosuppression"] = "Immunosuppression"
+table1$Variable[table1$Variable=="mod_sev_obesity"] = "Moderate/severe obesity"
+table1$Variable[table1$Variable=="diabetes"] = "Diabetes"
+table1$Variable[table1$Variable=="any_resp_dis"] = "Chronic respiratory disease (inc. asthma)"
+table1$Variable[table1$Variable=="chd"] = "Chronic heart disease"
+table1$Variable[table1$Variable=="cld"] = "Chronic liver disease"
+table1$Variable[table1$Variable=="asplenia"] = "Asplenia"
+table1$Variable[table1$Variable=="cancer"] = "Cancer (non-haematologic)"
+table1$Variable[table1$Variable=="haem_cancer"] = "Haematologic cancer"
+table1$Variable[table1$Variable=="non_kidney_transplant"] = "Organ transplant (non-kidney)"
+table1$Variable[table1$Variable=="chronic_neuro_dis_inc_sig_learn_dis"] = "Chronic neurological disease (inc. learning disability)"
+table1$Variable[table1$Variable=="sev_mental_ill"] = "Severe mental illness"
+table1$Variable[table1$Variable=="cev_other"] = "Clinically extremely vulnerable (other)"
+table1$Variable[table1$Variable=="chronic_kidney_disease_stages_3_5"] = "CKD3-5 diagnostic code"
 
 # Relabel groups for plotting
 # Demography
@@ -157,16 +172,14 @@ table1$Group[table1$Group=="imd"] = "IMD"
 table1$Group[table1$Group=="region"] = "Region"
 table1$Group[table1$Group=="jcvi_group"] = "JCVI group"
 table1$Group[table1$Group=="rural_urban_group"] = "Setting"
-table1$Group[table1$Group=="multimorb"] = "Comoborbidity count (non-CKD)"
+table1$Group[table1$Group=="ckd_7cat"] = "CKD subgroup"
+table1$Group[table1$Group=="time_between_vaccinations1_2"] = "Time between doses 1 and 2"
+table1$Group[table1$Group=="time_between_vaccinations2_3"] = "Time between doses 2 and 3"
 
 # Other
-table1$Group[table1$Variable %in% c("CKD diagnostic code", "Dialysis", "Kidney transplant", "CKD stage 3-5 code")] = "Clinical (CKD-related)"
-table1$Group[table1$Variable %in% c("Clinically extremely vulnerable", "Care home resident", "Health and social care worker", "End of life care", "Housebound", 
-                                    "Current or former smoker", "Asthma", "High or elevated blood pressure", "Shielding", "Immunosuppression", 
-                                    "Chronic respiratory disease", "Diabetes", "Chronic liver disease", "Chronic heart disease", "Asplenia", "Cancer",
-                                    "Haematologic cancer", "Obesity", "Chronic neurological disease (including learning disability)", "Severe mental illness", 
-                                    "Organ transplant (any)", "Organ transplant (non-kidney)", "Prior COVID")] = "Other"
-table1$Group[table1$Group=="time_between_vaccinations1_2"] = "Time between doses 1 and 2"
+table1$Group[!(table1$Group %in% c("Age", "Sex", "Ethnicity", "IMD", "Region", "JCVI group", "Setting", "CKD subgroup", 
+                                             "Time between doses 1 and 2", "Time between doses 2 and 3"))] = "Other"
+table1$Group[table1$Variable=="N"] = "N"
 
 # Redaction ----
 rounded_n_az = plyr::round_any(sum(data_cohort$vax12_type=="az-az"),5)
