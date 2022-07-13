b1dd5c4ef4d1fdfa3508e6571bab501ba6097f1f
eparker12 <e.parker12@imperial.ac.uk>
2022-07-13 08:25:15 +0100

    Simplified VE strata

--- analysis/VE/cox_preflight_VE.R
+++ analysis/VE/cox_preflight_VE.R
@@ -182,9 +182,9 @@ postvax_time <- data_cohort %>%
 surv = c("tstart", "tstop", "ind_outcome") 
 expo = "vax2_az"
 vars0 = "timesincevax_pw"
-vars1 = c("vax_day", "jcvi_region")
+vars1 = c("vax_day", "region")
 vars2_cont = "age"
-vars2_cat = c("sex", "imd", "ethnicity", "rural_urban_group", "ckd_3cat", "multimorb",
+vars2_cat = c("sex", "imd", "ethnicity", "rural_urban_group", "ckd_3cat", "multimorb", "jcvi_group",
               "learning_disability", "sev_mental_ill", "any_immunosuppression", "prior_covid_cat", "prevax_tests_cat")
 
 ## Drop ckd_3cat category from subgroup analyses
@@ -255,7 +255,7 @@ add_covars <- function(.data) {
   .data %>%
     left_join(
       data_cohort %>%
-        mutate(jcvi_region = as.character(glue("{jcvi_group}_{region}"))) %>%
+        #mutate(jcvi_region = as.character(glue("{jcvi_group}_{region}"))) %>%
         select(patient_id, all_of(c(expo, vars1, vars2_cont, vars2_cat))),
       by = "patient_id"
     ) %>%
@@ -510,10 +510,10 @@ if (db == "unmatched") {
   
   ## Set formula updates for calendar time vs person time models
   if (timescale == "persontime") {
-    formula1_update <- as.formula(". ~ . + strata(jcvi_region)*ns(vax_day, 3)")
+    formula1_update <- as.formula(". ~ . + strata(region)*ns(vax_day, 3)")
   } 
   if (timescale == "calendartime") {
-    formula1_update <- as.formula(". ~ . + strata(jcvi_region)")
+    formula1_update <- as.formula(". ~ . + strata(region)")
   }
 }
 
